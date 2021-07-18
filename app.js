a84d7d0d382b7ea38042c648fe33cc89fd2d0267
Shobhityadav99 <shobhit999000@gmail.com>
2021-07-19 02:29:29 +0530

    Basic api done

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET POST');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+    next();
+})
 
 app.use('/feed',feedRoutes);
 
