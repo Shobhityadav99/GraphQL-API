6ec54523c196e8beb4a256023eb5f3d260574479
Shobhityadav99 <shobhit999000@gmail.com>
2021-07-19 19:49:42 +0530

    Image upload successful

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,48 +1,74 @@
 const { validationResult } = require('express-validator/check');
+
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{
-            _id: '1',
-            title: 'First Post',
-            content: 'Content of first post!',
-            imageUrl: 'images/sample.png',
-            creator: {
-                name: 'Shobhit'
-            },
-            createdAt: new Date()
-        }]
+  Post.find()
+    .then(posts => {
+      res
+        .status(200)
+        .json({ message: 'Fetched posts successfully.', posts: posts });
     })
-}
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
 
 exports.createPost = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const error = new Error('Validation Failed, Entered data is incorrect');
-        error.statusCode = 422;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+  if (!req.file) {
+    const error = new Error('No image provided.');
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path.replace("\\","/");;
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: imageUrl,
+    creator: { name: 'Maximilian' }
+  });
+  post
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post.');
+        error.statusCode = 404;
         throw error;
-    }
-    const title = req.body.title;
-    const content = req.body.content;
-    const post = new Post({
-        title: title,
-        content: content,
-        imageUrl: 'images/sample.png',
-        creator: {
-            name: 'Shobhit'
-        },
+      }
+      res.status(200).json({ message: 'Post fetched.', post: post });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
     });
-    post.save().then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: 'Post created Successfully!!',
-            post: result
-        })
-    }).catch(err => {
-        if(!err.statusCode){
-            err.statusCode = 500;
-        }
-        next(err);
-    });
-}
+};
--- routes/feed.js
+++ routes/feed.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const { body } = require('express-validator/check');
 
-const router = express.Router();
-
 const feedController = require('../controllers/feed');
 
-router.get('/posts',feedController.getPosts);
+const router = express.Router();
 
-router.post('/post',[
-    body('title').trim().isLength({min: 5}),
-    body('content').trim().isLength({min: 5}),
+// GET /feed/posts
+router.get('/posts', feedController.getPosts);
 
-],feedController.createPost); 
+// POST /feed/post
+router.post(
+  '/post',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 7 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost
+);
+
+router.get('/post/:postId', feedController.getPost);
 
 module.exports = router;
