49e8cec7f8ff9ff2556eaefb6332dd30dec2f58d
Shobhityadav99 <shobhit999000@gmail.com>
2021-07-19 12:34:12 +0530

    Error handler added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -6,6 +7,7 @@ const feedRoutes = require('./routes/feed');
 
 const app = express();
 app.use(bodyParser.json());
+app.use('images',express.static(path.join(__dirname,'images')))
 
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin','*');
@@ -15,6 +17,13 @@ app.use((req,res,next) => {
 })
 
 app.use('/feed',feedRoutes);
+
+app.use((error,req,res,next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message: message});
+})
 
 mongoose.connect(
     'mongodb+srv://shobhit:shobhit@cluster0.eoc3v.mongodb.net/messages?retryWrites=true&w=majority'
--- controllers/feed.js
+++ controllers/feed.js
@@ -19,10 +19,9 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(422).json({
-            message: 'Validation Failed, Entered data is incorrect',
-            errors: errors.array()
-        })
+        const error = new Error('Validation Failed, Entered data is incorrect');
+        error.statusCode = 422;
+        throw error;
     }
     const title = req.body.title;
     const content = req.body.content;
@@ -40,5 +39,10 @@ exports.createPost = (req, res, next) => {
             message: 'Post created Successfully!!',
             post: result
         })
-    }).catch(err => console.log(err));
+    }).catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    });
 }
