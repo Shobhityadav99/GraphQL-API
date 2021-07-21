57487c80e2f0c3dc49e8dd5dee57a7b73f32a333
Shobhityadav99 <shobhit999000@gmail.com>
2021-07-21 17:46:03 +0530

    User model added

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Post = require('./post');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email : {
+        type: String,
+        required: true,
+    },
+    password : {
+        type: String,
+        required: true,
+    },
+    name : {
+        type: String,
+        required: true,
+    },
+    status : {
+        type: String,
+        required: true,
+    },
+    posts : [{
+        type: Schema.Types.ObjectId,
+        ref: Post
+    }],
+});
+
+module.exports = model('User',userSchema);
