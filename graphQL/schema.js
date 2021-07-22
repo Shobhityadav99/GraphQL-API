27bb9fb91b2577c6026687c05ae429d866e14c31
Shobhityadav99 <shobhit999000@gmail.com>
2021-07-22 20:04:31 +0530

    Schema done

--- graphQL/schema.js
+++ graphQL/schema.js
@@ -1,16 +1,32 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-    type TestData {
-        text: String!
-        views: Int!
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: String!
+        createdAt: String!
+        updatedAt: String!
     }
-
-    type RootQuery{
-        hello: TestData!
+    type User {
+        _id: ID!
+        email: String!
+        name: String!
+        password: String
+        status: String!
+        posts: [Post!]!
     }
-
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+    }
     schema {
-        query: RootQuery
+        query: RootMutation
     }
 `)
